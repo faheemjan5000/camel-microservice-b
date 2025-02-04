182d99dd06045d11d7c54938e9fe4a6761c8e530
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2025-02-04 09:28:03 +0100

    first commit

---
+++ src/test/java/com/micro/camel/microservice/b/CamelMicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.camel.microservice.b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
